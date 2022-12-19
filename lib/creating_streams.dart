09c54a713fa64b35aa800fdb8fb9ebe533fa63bd
Abdumutalibovo <odilbekjuraev07@gmail.com>
2022-12-19 15:56:27 +0500

    async* and Transforming Streams, By Stream Controller

--- lib/creating_streams.dart
+++ lib/creating_streams.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 main() async {
   //1 async*
 
@@ -20,27 +22,60 @@ main() async {
   //   print(event);
   // });
 
-  //Transforming Streams
+  //2 Transforming Streams
 
-  Stream<int> myNumberCreator() async* {
-    for (int i = 1; i < 10; i++) {
-      await Future.delayed(const Duration(seconds: 1));
-      yield i;
-    }
-  }
-
-  Stream<int> myStream = myNumberCreator();
-
-  Stream<int> newStream = myStream.map((event) => event * event);
-
-  Stream<int> secondStream = myStream.where((event) => event % 2 == 0)
-  .expand((element) => [element+1,element+2,element+3]);
-
-  // newStream.listen((event) {
+  // Stream<int> myNumberCreator() async* {
+  //   for (int i = 1; i < 10; i++) {
+  //     await Future.delayed(const Duration(seconds: 1));
+  //     yield i;
+  //   }
+  // }
+  //
+  // Stream<int> myStream = myNumberCreator();
+  //
+  // Stream<int> newStream = myStream.map((event) => event * event);
+  //
+  // Stream<int> secondStream = myStream.where((event) => event % 2 == 0)
+  // .expand((element) => [element+1,element+2,element+3]);
+  //
+  // // newStream.listen((event) {
+  // //   print(event);
+  // // });
+  //
+  // secondStream.listen((event) {
   //   print(event);
   // });
 
-  secondStream.listen((event) {
+  //3 By Stream Controller
+
+  StreamController<int> streamController = StreamController<int>();
+
+  int k = 10;
+
+  // while(k>0){
+  //   await Future.delayed(const Duration(seconds: 1));
+  //   k--;
+  //   streamController.sink.add(k);
+  // }
+
+  streamController.add(k);
+
+  var st = streamController.stream.asBroadcastStream();
+
+  late StreamSubscription subscription;
+
+  subscription = st.listen((event) {
+    print(event);
+    subscription.pause(Future.delayed(
+      Duration(seconds: 1),
+    ));
+  });
+
+  await Future.delayed(Duration(seconds: 1));
+
+  subscription.resume();
+
+  st.listen((event) {
     print(event);
   });
 }
